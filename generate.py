17b0285690c2349928d99a4b273f3f91109f771e
mZake <zakeplaybr@gmail.com>
2026-06-04 15:06:19 -0300

    Add generator for link rule

--- generate.py
+++ generate.py
@@ -30,14 +30,30 @@ class Generator:
         for key, value in kwargs.items():
             self.stream.write(f"  {key} = {value}\n")
 
+    def write_build_list(self, rule: str, files_out: list, files_in: list, **kwargs):
+        self.stream.write("build")
+        for file_out in files_out:
+            self.stream.write(f" {file_out}")
+
+        self.stream.write(f": {rule}")
+        for file_in in files_in:
+            self.stream.write(f" {file_in}")
+
+        self.stream.write("\n")
+        for key, value in kwargs.items():
+            self.stream.write(f"  {key} = {value}\n")
+
     def break_line(self):
         self.stream.write("\n")
 
 with Generator("build.ninja") as gen:
     gen.write_var("gcc", "arm-none-eabi-gcc")
     gen.write_var("as", "arm-none-eabi-as")
+    gen.write_var("ld", "arm-none-eabi-ld")
     gen.write_var("preproc", "build/tools/preproc")
     gen.write_var("gbagfx", "build/tools/gbagfx")
+    gen.break_line()
+    gen.write_var("ldflags", "-T linker.ld")
     gen.break_line()
 
     gen.write_rule("cc", command="$preproc $in charmap.txt | $gcc $cflags -MD -MF $out.d -xc -c -o $out -", depfile="$out.d")
@@ -47,6 +63,9 @@ with Generator("build.ninja") as gen:
     gen.break_line()
 
     gen.write_rule("gfx", command="$gbagfx $in $out")
+    gen.break_line()
+
+    gen.write_rule("link", command="$ld $ldflags -o $out $in")
     gen.break_line()
 
     c_files = glob.glob("src/**/*.c", recursive=True)
@@ -80,3 +99,9 @@ with Generator("build.ninja") as gen:
     for asm_file in asm_files:
         out_file = os.path.join("build", asm_file) + ".o"
         gen.write_build("asm", out_file, asm_file)
+
+    c_obj_files = glob.glob("build/src/**/*.o", recursive=True)
+    asm_obj_files = glob.glob("build/asm/**/*.o", recursive=True)
+    obj_files = c_obj_files + asm_obj_files
+
+    gen.write_build_list("link", ["build/linked.o"], obj_files)
