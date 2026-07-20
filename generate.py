91b6b5928a937d4619f90c6a4c06a8bc876e4d70
mZake <zakeplaybr@gmail.com>
2026-07-20 13:09:51 -0300

    Generator: refactor tool integration
    Use subninja to integrate tool targets into the top-level build file,
    so Ninja knows the dependencies of each tool and thus when they actually
    need to be rebuilt.
    
    Go back to expressing tool targets as implicit dependencies. Tools are
    always built before their dependent targets, and changes to them cause
    those targets to rebuild as well. This is now viable because Ninja
    rebuilds tool targets only when necessary, since it knows their
    dependencies now.

--- generate.py
+++ generate.py
@@ -13,17 +13,13 @@ OFFSET_TO_INSERT = 0x1400000
 BASE_ROM_FILE = "base.gba"
 OUT_ROM_FILE = "out.gba"
 
-BUILD_DIR = "build"
+ASM_DIR = "asm"
 GFX_DIR = "graphics"
 INC_DIR = "include"
+SRC_DIR = "src"
+BUILD_DIR = "build"
 PATCH_DIR = "patch"
 TOOLS_DIR = "tools"
-
-C_SRC_DIR = "src"
-ASM_SRC_DIR = "asm"
-
-C_BUILD_DIR = f"{BUILD_DIR}/{C_SRC_DIR}"
-ASM_BUILD_DIR = f"{BUILD_DIR}/{ASM_SRC_DIR}"
 
 BLOB_OBJECT = f"{BUILD_DIR}/blob.o"
 
@@ -59,6 +55,12 @@ class Writer:
         self._line(f"pool {name}")
         self._line(f"depth = {depth}", indent=2)
         self.newline()
+
+    def include(self, path: str):
+        self._line(f"include {path}")
+
+    def subninja(self, path: str):
+        self._line(f"subninja {path}")
 
     def build(
         self,
@@ -127,10 +129,6 @@ def fatal(message: str):
     sys.stderr.write("\n")
     sys.exit(1)
 
-def configure_directories():
-    os.makedirs(C_BUILD_DIR, exist_ok=True)
-    os.makedirs(ASM_BUILD_DIR, exist_ok=True)
-
 def configure_toolchain():
     toolchain_path = os.getenv("DEVKITARM")
     if not toolchain_path:
@@ -148,7 +146,7 @@ def configure_toolchain():
 
 def collect_files(directory: str, extension: str) -> list[str]:
     matches = glob.glob(f"{directory}/**/*{extension}", recursive=True)
-    return [os.path.relpath(x, directory) for x in matches]
+    return matches
 
 def derive_files(inputs: str | list[str], pattern: str) -> list[str]:
     outputs = list()
@@ -160,39 +158,35 @@ def derive_files(inputs: str | list[str], pattern: str) -> list[str]:
     return outputs
 
 def main():
-    configure_directories()
+    # configure_directories()
     configure_toolchain()
     # configure_tools()
 
-    png_inputs = collect_files(GFX_DIR, ".png")
-    c_inputs = collect_files(C_SRC_DIR, ".c")
-    asm_inputs = collect_files(ASM_SRC_DIR, ".s")
-
     # Inputs
-    png_files = derive_files(png_inputs, f"{GFX_DIR}/%.png")
-    c_sources = derive_files(c_inputs, f"{C_SRC_DIR}/%.c")
-    asm_sources = derive_files(asm_inputs, f"{ASM_SRC_DIR}/%.s")
+    png_files   = collect_files(GFX_DIR, ".png")
+    c_sources   = collect_files(SRC_DIR, ".c")
+    asm_sources = collect_files(ASM_DIR, ".s")
 
     # Outputs
-    bpp1_files = derive_files(png_inputs, f"{GFX_DIR}/%.1bpp")
-    bpp4_files = derive_files(png_inputs, f"{GFX_DIR}/%.4bpp")
-    bpp8_files = derive_files(png_inputs, f"{GFX_DIR}/%.8bpp")
+    bpp1_files = derive_files(png_files, "%.1bpp")
+    bpp4_files = derive_files(png_files, "%.4bpp")
+    bpp8_files = derive_files(png_files, "%.8bpp")
 
-    bpp1_lz_files = derive_files(png_inputs, f"{GFX_DIR}/%.1bpp.lz")
-    bpp4_lz_files = derive_files(png_inputs, f"{GFX_DIR}/%.4bpp.lz")
-    bpp8_lz_files = derive_files(png_inputs, f"{GFX_DIR}/%.8bpp.lz")
+    bpp1_lz_files = derive_files(png_files, "%.1bpp.lz")
+    bpp4_lz_files = derive_files(png_files, "%.4bpp.lz")
+    bpp8_lz_files = derive_files(png_files, "%.8bpp.lz")
 
-    c_objects = derive_files(c_inputs, f"{C_BUILD_DIR}/%.o")
-    asm_objects = derive_files(asm_inputs, f"{ASM_BUILD_DIR}/%.o")
+    c_objects   = derive_files(c_sources,   f"{BUILD_DIR}/%.o")
+    asm_objects = derive_files(asm_sources, f"{BUILD_DIR}/%.o")
     all_objects = c_objects + asm_objects
+
+    c_depfiles   = derive_files(c_sources,   f"{BUILD_DIR}/%.o.d")
+    asm_depfiles = derive_files(asm_sources, f"{BUILD_DIR}/%.o.d")
 
     # Patches are compiled for the host machine. They are getting placed
     # here to avoid ambiguity.    
     patch_sources = collect_files(PATCH_DIR, ".cpp")
     patch_headers = collect_files(PATCH_DIR, ".hpp")
-
-    c_depfiles = derive_files(c_inputs, f"{C_BUILD_DIR}/%.o.d")
-    asm_depfiles = derive_files(asm_inputs, f"{ASM_BUILD_DIR}/%.o.d")
 
     with open(f"{PATCH_DIR}/build.ninja", "w", encoding="utf-8") as stream:
         writer = Writer(stream)
@@ -202,12 +196,10 @@ def main():
         writer.variable("cxxflags", f"-std=c++17 -O2 -Wall -Wextra -I{INC_DIR}")
         writer.newline()
 
-        writer.rule("cxx", command="$cxx $cxxflags $in -o $out")
-
-        writer.build("cxx",
+        writer.build("host_cxx",
                      inputs=patch_sources,
                      implicit_inputs=patch_headers,
-                     outputs="patchbin")
+                     outputs=PATCHBIN)
 
     with open("build.ninja", "w", encoding="utf-8") as stream:
         writer = Writer(stream)
@@ -224,48 +216,44 @@ def main():
         writer.variable("wav2agb",  WAV2AGB)
         writer.newline()
         writer.variable("cflags", "-mthumb -mthumb-interwork -march=armv4t -mtune=arm7tdmi -mabi=apcs-gnu -mlong-calls -O2 -fno-toplevel-reorder")
-        writer.variable("asflags", f"-mthumb -mthumb-interwork -march=armv4t -mcpu=arm7tdmi -meabi=gnu -I {ASM_SRC_DIR}")
+        writer.variable("asflags", f"-mthumb -mthumb-interwork -march=armv4t -mcpu=arm7tdmi -meabi=gnu -I {ASM_DIR}")
         writer.variable("ldflags", f"-T linker.ld BPRE.ld -r --defsym=BLOB_BEGIN=0x{ADDRESS_TO_INSERT:08X}")
         writer.variable("cppflags", f"-I {INC_DIR}")
         writer.newline()
 
-        writer.pool("ninja_pool", depth=1)
-
-        writer.rule("ninja",
-                    command="ninja -C $in",
-                    pool="ninja_pool")
-
         writer.rule("cc", command="$cc -E $cppflags $in | $preproc -i $in charmap.txt | $cc $cflags -xc -o $out -c -", depfile="$out.d")
         writer.rule("as", command="$as $asflags -c $in -o $out", depfile="$out.d")
         writer.rule("ld", command="$ld $ldflags -o $out $in")
+        writer.rule("host_cc", command="$cc $cflags $in -o $out")
+        writer.rule("host_cxx", command="$cxx $cxxflags $in -o $out")
         writer.rule("gbagfx", command="$gbagfx $in $out")
         writer.rule("patchbin", command=f"$patchbin $in $out")
-        writer.rule("scaninc", command=f"$scaninc -M $out -I {INC_DIR} -I {ASM_SRC_DIR} $in")
+        writer.rule("scaninc", command=f"$scaninc -M $out -I {INC_DIR} -I {ASM_DIR} $in")
 
-        writer.build("ninja", f"{TOOLS_DIR}/gbagfx",  "$gbagfx")
-        writer.build("ninja", f"{TOOLS_DIR}/mid2agb", "$mid2agb")
-        writer.build("ninja", PATCH_DIR,              "$patchbin")
-        writer.build("ninja", f"{TOOLS_DIR}/preproc", "$preproc")
-        writer.build("ninja", f"{TOOLS_DIR}/scaninc", "$scaninc")
-        writer.build("ninja", f"{TOOLS_DIR}/wav2agb", "$wav2agb")
+        writer.subninja(f"{TOOLS_DIR}/gbagfx/build.ninja")
+        writer.subninja(f"{TOOLS_DIR}/mid2agb/build.ninja")
+        writer.subninja(f"{PATCH_DIR}/build.ninja")
+        writer.subninja(f"{TOOLS_DIR}/preproc/build.ninja")
+        writer.subninja(f"{TOOLS_DIR}/scaninc/build.ninja")
+        writer.subninja(f"{TOOLS_DIR}/wav2agb/build.ninja")
         writer.newline()
 
-        writer.build_group("gbagfx", png_files, bpp1_files, order_only="$gbagfx")
-        writer.build_group("gbagfx", png_files, bpp4_files, order_only="$gbagfx")
-        writer.build_group("gbagfx", png_files, bpp8_files, order_only="$gbagfx")
+        writer.build_group("gbagfx", png_files, bpp1_files, implicit_inputs="$gbagfx")
+        writer.build_group("gbagfx", png_files, bpp4_files, implicit_inputs="$gbagfx")
+        writer.build_group("gbagfx", png_files, bpp8_files, implicit_inputs="$gbagfx")
 
-        writer.build_group("gbagfx", bpp1_files, bpp1_lz_files, order_only="$gbagfx")
-        writer.build_group("gbagfx", bpp4_files, bpp4_lz_files, order_only="$gbagfx")
-        writer.build_group("gbagfx", bpp8_files, bpp8_lz_files, order_only="$gbagfx")
+        writer.build_group("gbagfx", bpp1_files, bpp1_lz_files, implicit_inputs="$gbagfx")
+        writer.build_group("gbagfx", bpp4_files, bpp4_lz_files, implicit_inputs="$gbagfx")
+        writer.build_group("gbagfx", bpp8_files, bpp8_lz_files, implicit_inputs="$gbagfx")
 
-        writer.build_group("cc", c_sources, c_objects, order_only="$preproc")
+        writer.build_group("cc", c_sources, c_objects, implicit_inputs="$preproc")
         writer.build_group("as", asm_sources, asm_objects)
 
-        writer.build_group("scaninc", c_sources, c_depfiles, order_only="$scaninc")
-        writer.build_group("scaninc", asm_sources, asm_depfiles, order_only="$scaninc")
+        writer.build_group("scaninc", c_sources, c_depfiles, implicit_inputs="$scaninc")
+        writer.build_group("scaninc", asm_sources, asm_depfiles, implicit_inputs="$scaninc")
 
         if all_objects:
             writer.build("ld", all_objects, BLOB_OBJECT)
-            writer.build("patchbin", [BASE_ROM_FILE, BLOB_OBJECT], OUT_ROM_FILE, order_only="$patchbin")
+            writer.build("patchbin", [BASE_ROM_FILE, BLOB_OBJECT], OUT_ROM_FILE, implicit_inputs="$patchbin")
 
 if __name__ == "__main__": main()
