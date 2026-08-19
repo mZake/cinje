cd62000db69563b835b21fd11a3f187632d133e2
mZake <zakeplaybr@gmail.com>
2026-08-19 09:41:50 -0300

    Patchbin: use variables to store paths in test.sh
    Improve readability and reusability.

--- patch/tests/test.sh
+++ patch/tests/test.sh
@@ -8,12 +8,20 @@ CXX="${CXX:-c++}"
 
 CXXFLAGS="-std=c++17 -I .. -Wall -Wextra"
 
+INPUT_BINARY="${HERE}/data/input.bin"
+ELF_OBJECT="${HERE}/data/elf.o"
+OUTPUT_BINARY="${HERE}/data/output.bin"
+
 build-test() {
-    ${CXX} ${CXXFLAGS} ${1} -o ${2}
+    PROGRAM="${HERE}/${2}"
+
+    ${CXX} ${CXXFLAGS} ${1} -o "${PROGRAM}"
 }
 
 expect-success() {
-    if "${1}" "${HERE}/data/input.bin" "${HERE}/data/elf.o" "${HERE}/data/output.bin"; then
+    PROGRAM="${HERE}/${1}"
+
+    if "${PROGRAM}" "${INPUT_BINARY}" "${ELF_OBJECT}" "${OUTPUT_BINARY}"; then
         echo -e "\e[32;1mTest '${2}' succeeded"
     else
         echo -e "\e[31;1mTest '${2}' failed"
@@ -22,7 +30,9 @@ expect-success() {
 }
 
 expect-failure() {
-    if ! "${1}" "${HERE}/data/input.bin" "${HERE}/data/elf.o" "${HERE}/data/output.bin"; then
+    PROGRAM="${HERE}/${1}"
+
+    if ! "${PROGRAM}" "${INPUT_BINARY}" "${ELF_OBJECT}" "${OUTPUT_BINARY}"; then
         echo -e "\e[32;1mTest '${2}' succeeded"
     else
         echo -e "\e[31;1mTest '${2}' failed"
@@ -30,8 +40,8 @@ expect-failure() {
     echo -e "\e[0m"
 }
 
-build-test good_patches.cpp "${HERE}/good-patches"
-build-test bad_patches.cpp "${HERE}/bad-patches"
+build-test good_patches.cpp good-patches
+build-test bad_patches.cpp bad-patches
 
-expect-success "${HERE}/good-patches" "Good Patches"
-expect-failure "${HERE}/bad-patches" "Bad Patches"
+expect-success good-patches "Good Patches"
+expect-failure bad-patches "Bad Patches"
